6c9029f2a4f3ef9f3d7e8683dee9b17f35455fdf
lon <longhaohe@gmail.com>
2017-02-13 22:33:02 +0800

    Add commander

--- bin.js
+++ bin.js
@@ -21,24 +21,41 @@ if (!supportAsync) {
     console.log(stderr)
   })
 } else {
+  const pkg = require('./package.json')
+  const program = require('./commander')
+  program.version(pkg.version)
+    .usage('[options]')
+    .option('-h, --help', '显示帮助信息')
+    .option('-c, --config <path>', '配置文件路径，默认："./mark.config.js"', './mark.config.js')
+    .option('-s, --src <path>', '输入文件名或文件夹，默认："./markdown"', './markdown')
+    .option('-d, --dst <path>', '输出文件名或文件夹，默认："./html"', './html')
+    .option('-t, --theme <path>', '主题文件夹，默认："./theme"', './theme')
+    .option('-p, --public-path <path>', '输出相对路径，默认：""(空字符串)', '')
+    .parse(process.argv)
+
   const fs = require('fs')
   const path = require('path')
-  const marksite = require('./index')
+  const marksite = require('./src/marksite')
+  const {merge} = require('./src/common')
 
-  const configName = './mark.config.js'
+  const {src, dst, theme, publicPath} = program
+  const argvConfig = {src, dst, theme, publicPath}
 
+  const configName = program.config
   fs.access(
     configName,
     fs.constants.F_OK,
     error => {
-      let config = {}
+      let fileConfig = {}
       if (!error) {
         const currentPath = process.cwd()
         const configPath = path.join(currentPath, configName)
         try {
-          config = require(configPath)
+          fileConfig = require(configPath)
         } catch (error) {}
       }
+
+      const config = merge(fileConfig, argvConfig)
       marksite(config)
       .then(markedFiles => {
         console.log('done!')
